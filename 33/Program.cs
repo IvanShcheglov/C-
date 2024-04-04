0fb72535c5e03f14ef5840f0b041f4f607eda85b
IvanShcheglov <sheglov.ivan@mail.ru>
2024-04-04 19:12:16 +0300

    3 задание

---
+++ 33/Program.cs
@@ -0,0 +1,17 @@
+﻿ 
+
+
+    void print(int [] Arr, int n) 
+    { 
+      if (n > 0) 
+      { 
+      	Console.Write(Arr[n] + " "); 
+      	print(Arr, n - 1); 
+      } 
+      else Console.WriteLine(Arr[0]); 
+    } 
+    
+      int [] num = {2, 3, 5, 550, 100, 3004}; 
+      print(num, num.Length - 1); 
+    
+  
